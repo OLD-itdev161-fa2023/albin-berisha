08ce02545c0f8f9e004311294c7b39326da36a08
achooStorm <114879202+achooStorm@users.noreply.github.com>
2022-10-03 15:59:07 -0500

    Finally Posteman error fixed

--- server.js
+++ server.js
@@ -1,76 +1,50 @@
 import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
-import cors  from 'cors';
-import bcrypt from 'bcryptjs';
-import User from './models/User';
 
-
-//Initialize express application
+// Initialize express application
 const app = express();
-//Connect Database
+
+// Connect database
 connectDatabase();
 
-//Configure MiddleWare
-app.use(express.json({extended:false}));
-app.use(
-    cors({
-        origin: 'http://localhost:3000'
-    })
-);
+// Configure Middleware
+app.use(express.json({ extended: false }));
 
-
-//API endpoints
+// API endpoints
 /**
- * @route GET/
+ * @route GET /
  * @desc Test endpoint
  */
 app.get('/', (req, res) =>
-    res.send('http get request sent to root api endpoint')
+  res.send('http get request sent to root api endpoint')
 );
 
 /**
  * @route POST api/users
  * @desc Register user
  */
-
 app.post(
-    '/api/users',
-    [
-        check('name', 'Please enter your name').not().isEmpty(),
-        check('email', 'Please enter a valid email').isEmail(),
-        check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
-
-    ],
-        async(req, res)=> {
-            const errors = validationResult(req);
-            if(!errors.isEmpty()){
-                return res.status(422).json({errors: errors.array()});
-            }else{
-             const { name, email, password } = req.body;
-             try {
-                let user = await User.findOne({email: email});
-                if(user) {
-                    return res
-                    .status(400)
-                    .json({ errors: [{ msg: 'User already exists' }] });
-                }
-
-                user = new User({
-                    name: name,
-                    email: email,
-                    password: password
-                });
-
-                const salt  = await bcrypt.genSalt(10);
-                user.password = await bcrypt.hash(password, salt);
-
-                await user.save();
-                res.send('User successfully registered');
-             } catch (error){
-                res.status(500).send('Server error');
-             }
-        }
-    }    
+  '/api/users',
+  [
+    check('name', 'Please enter your name')
+      .not()
+      .isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    } else {
+      return res.send(req.body);
+    }
+  }
 );
 
+// Connection listener
+app.listen(3000, () => console.log(`Express server running on port 3000`));
