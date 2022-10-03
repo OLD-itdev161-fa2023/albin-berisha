8c6dbef6cb4b97150118605494c13a5a3948d38a
achooStorm <114879202+achooStorm@users.noreply.github.com>
2022-10-03 15:49:38 -0500

    Updating Posteman

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors  from 'cors';
+import bcrypt from 'bcryptjs';
+import User from './models/User';
+
+
 //Initialize express application
 const app = express();
 //Connect Database
@@ -14,6 +18,7 @@ app.use(
         origin: 'http://localhost:3000'
     })
 );
+
 
 //API endpoints
 /**
@@ -37,16 +42,35 @@ app.post(
         check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
 
     ],
-        (req, res)=> {
+        async(req, res)=> {
             const errors = validationResult(req);
             if(!errors.isEmpty()){
                 return res.status(422).json({errors: errors.array()});
             }else{
-                return res.send(req.body);
-            }
-    
-});
+             const { name, email, password } = req.body;
+             try {
+                let user = await User.findOne({email: email});
+                if(user) {
+                    return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'User already exists' }] });
+                }
 
-//Connection listener 
-const port = 5000;
-app.listen(port, () => console.log (`Express server running on port ${port}`));
+                user = new User({
+                    name: name,
+                    email: email,
+                    password: password
+                });
+
+                const salt  = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+
+                await user.save();
+                res.send('User successfully registered');
+             } catch (error){
+                res.status(500).send('Server error');
+             }
+        }
+    }    
+);
+
