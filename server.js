5620ff825b3119c44c1d47f1f67ee76475563c3d
achooStorm <114879202+achooStorm@users.noreply.github.com>
2022-10-03 19:18:18 -0500

    having some extra fun

--- server.js
+++ server.js
@@ -1,52 +1,41 @@
 import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
-import cors  from 'cors';
-//Initialize express application
+
 const app = express();
-//Connect Database
+
 connectDatabase();
 
-//Configure MiddleWare
-app.use(express.json({extended:false}));
-app.use(
-    cors({
-        origin: 'http://localhost:3000'
-    })
-);
+app.use(express.json({extended: false}));
+/*
+@route 
+@desc
+*/
 
-//API endpoints
-/**
- * @route GET/
- * @desc Test endpoint
- */
 app.get('/', (req, res) =>
-    res.send('http get request sent to root api endpoint')
+res.send('http get request sent to root api endpoint')
 );
 
-/**
- * @route POST api/users
- * @desc Register user
- */
+/*
+@route 
+@desc
+*/
 
 app.post(
-    '/api/users',
+    '/api/users', 
     [
         check('name', 'Please enter your name').not().isEmpty(),
         check('email', 'Please enter a valid email').isEmail(),
-        check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
-
+        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
     ],
-        (req, res)=> {
-            const errors = validationResult(req);
-            if(!errors.isEmpty()){
-                return res.status(422).json({errors: errors.array()});
-            }else{
-                return res.send(req.body);
-            }
-    
+    (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    else {
+        res.send(req.body); 
+    }
 });
-
-//Connection listener 
 const port = 5000;
-app.listen(port, () => console.log (`Express server running on port ${port}`));
+app.listen(port, () => console.log(`Express server running on port ${port}`));
